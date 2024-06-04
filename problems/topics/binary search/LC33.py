6450ce1542d8962db98b9507963f9ddbdfc73d82
khangdang <dlqkhang8@gmail.com>
2024-06-04 22:05:57 +0700

    problems

--- problems/topics/binary search/LC33.py
+++ problems/topics/binary search/LC33.py
@@ -5,25 +5,22 @@ from typing import List
 
 
 class Solution:
-    def search(self, a: List[int], t: int) -> bool:
+    def search(self, a: List[int], t: int) -> int:
         n = len(a)
-        while n > 1 and a[0] == a[n - 1]:
-            n -= 1
-
         l, r = -1, n
         while r - l > 1:
             mid = l + (r - l) // 2
-            if (a[0] <= t and a[0] <= a[mid] <= t) or not (a[0] <= t or a[0] > a[mid] > t):
+            if (a[0] <= t and a[0] <= a[mid] <= t) or (a[0] > t and not (a[0] > a[mid] > t)):
                 l = mid
             else:
                 r = mid
 
         if l in [n, -1] or a[l] != t:
-            return False
+            return -1
 
-        return True
+        return l
 
 
 if __name__ == '__main__':
     sol = Solution()
-    print(sol.search([2], 2))
+    print(sol.search([4, 5, 6, 7, 0, 1, 2], 6))
