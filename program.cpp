403680d1adeae55cd38c56e547582e0286f938a2
khangdang <dlqkhang8@gmail.com>
2025-03-12 20:15:28 +0700

    problems

--- program.cpp
+++ program.cpp
@@ -1,6 +1,6 @@
 #include <bits/stdc++.h>
-#include <ext/pb_ds/assoc_container.hpp>
 #include <ext/pb_ds/tree_policy.hpp>
+#include <utility>
 
 using namespace std;
 using namespace __gnu_pbds;
@@ -23,125 +23,46 @@ void IN_OUT() {
 #endif
 }
 
-struct CHash {
-    static uint64_t splitmix64(uint64_t x) {
-        // http://xorshift.di.unimi.it/splitmix64.c
-        x += 0x9e3779b97f4a7c15;
-        x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;
-        x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
-        return x ^ (x >> 31);
-    }
+string solve(ll n, string s, ll k) {
+    for (int i = 0; i < pow(2, n - 1); i++) {
+        string ans, curr;
+        curr.push_back(s[0]);
 
-    size_t operator()(uint64_t x) const {
-        static const uint64_t FIXED_RANDOM =
-                chrono::steady_clock::now().time_since_epoch().count();
-        return splitmix64(x + FIXED_RANDOM);
-    }
-};
-
-long parent[MAX];
-long set_size[MAX];
-
-class DSU {
-public:
-
-    void make_set(ll a) {
-        parent[a] = a;
-        set_size[a] = 1;
-    }
-
-    ll find_set(ll a) {
-        if (parent[a] == a)
-            return a;
-
-        parent[a] = find_set(parent[a]);
-        return parent[a];
-    }
-
-    void merge_set(ll a, ll b) {
-        auto lt = find_set(a);
-        auto gt = find_set(b);
-        if (lt == gt)
-            return;
-
-        if (set_size[lt] > set_size[gt])
-            swap(lt, gt);
-
-        parent[lt] = gt;
-        set_size[gt] += set_size[lt];
-    }
-};
-
-void solve(const vector<ll> &a, int n, int q) {
-    DSU dsu;
-    unordered_map<ll, ll> valueToRep;
-    unordered_map<ll, ll> repToValue;
-    for (int i = 0; i < n; i++) {
-        dsu.make_set(i + 1);
-        if (valueToRep.find(a[i]) != valueToRep.end())
-            dsu.merge_set(i + 1, valueToRep[a[i]]);
-
-        auto rep = dsu.find_set(i + 1);
-        valueToRep[a[i]] = rep;
-        repToValue[rep] = a[i];
-    }
-
-    for (int i = 0; i < q; i++) {
-        int type;
-        cin >> type;
-
-        if (type == 2) {
-            int index;
-            cin >> index;
-
-            cout << repToValue[dsu.find_set(index)] << "\n";
-            continue;
+        ll sum = 0;
+        for (int j = 0; j < n - 1; j++) {
+            if (i >> j & 1) {
+                sum += atoi(curr.c_str());
+                ans += curr + "+";
+                curr = "";
+            }
+            if (sum > k)
+                break;
+            curr.push_back(s[j + 1]);
         }
-
-        ll x, y;
-        cin >> x >> y;
-        if (x == y || valueToRep[x] == 0) {
-            // cout << "no: " << x << " " << y << " " << valueToRep[x] << "\n";
-            continue;
-        }
-
-        if (valueToRep[y] == 0) {
-            valueToRep[y] = valueToRep[x];
-            valueToRep[x] = 0;
-
-            repToValue[valueToRep[y]] = y;
-        } else {
-            auto rep = valueToRep[x];
-            dsu.merge_set(valueToRep[x], valueToRep[y]);
-
-            rep = dsu.find_set(rep);
-            valueToRep[x] = 0;
-            valueToRep[y] = rep;
-
-            repToValue[rep] = y;
-        }
-
-        // cout << "bbbb " << x << " " << y << " " << dsu.find_set(x) << "\n";
+        ans += curr;
+        sum += atoi(curr.c_str());
+        if (sum == k)
+            return ans;
     }
+    return "";
 }
 
 int main() {
     fastio();
     IN_OUT();
 
-    int t;
+    ll t;
     cin >> t;
 
-    for (int i = 0; i < t; i++) {
-        int n, q;
-        cin >> n >> q;
+    while (t > 0) {
+        t--;
 
-        vector<ll> a(n);
-        for (int j = 0; j < n; j++)
-            cin >> a[j];
+        string s;
+        ll k;
+        cin >> s >> k;
 
-        cout << "Case " << i + 1 << ":" << "\n";
-        solve(a, n, q);
+        cout << solve(s.size(), s, k) << "\n";
     }
+
     return 0;
 }
--- program.py
+++ program.py
@@ -1,50 +1,71 @@
 from collections import deque, defaultdict, Counter
 from dataclasses import dataclass, field
 from functools import cache, lru_cache
-from heapq import heappop, heappush, heapify
-from itertools import count
-from math import log10, floor, sqrt, floor, ceil, lcm
+from heapq import heappop, heappush, heapify, merge
+from math import log10, floor, sqrt, floor, ceil, lcm, trunc
 from typing import List, Deque, Tuple, Dict
 
+
 class Solution:
-    def minCost(self, grid: List[List[int]]) -> int:
-        n, m = len(grid), len(grid[0])
-        directions = [(0, 0), (0, 1), (0, -1), (1, 0), (-1, 0)]
+    def mostProfitablePath(self, edges: List[List[int]], bob: int, cost: List[int]) -> int:
+        n = len(edges) + 1
 
-        dis = [[10 ** 9] * m for _ in range(n)]
-        dis[0][0] = 0
+        adj = [[] for _ in range(n)]
+        for u, v in edges:
+            adj[u].append(v)
+            adj[v].append(u)
 
-        queue = deque()
-        queue.append((0, 0, 0))
-        while queue:
-            (uw, i, j) = queue.popleft()
-            if (i, j) == (n - 1, m - 1):
-                return uw
+        bob_dep = [None] * n
 
-            for dx, dy in [(0, -1), (0, 1), (-1, 0), (1, 0)]:
-                x, y = i + dx, j + dy
-                if not (0 <= x < n and 0 <= y < m):
+        def dfs(par, node, time, cum):
+            nonlocal bob_dep, cost
+            if node == 0:
+                return True
+
+            for nxt in adj[node]:
+                if nxt == par:
                     continue
 
-                vw = uw
-                if directions[grid[i][j]] != (dx, dy):
-                    vw += 1
+                if dfs(node, nxt, time + 1, cum + cost[nxt]):
+                    bob_dep[nxt] = time + 1
+                    return True
 
-                if dis[x][y] > vw:
-                    dis[x][y] = vw
-                    if vw == uw:
-                        queue.appendleft((vw, x, y))
-                    else:
-                        queue.append((vw, x, y))
+        bob_dep[bob] = 0
+        dfs(-1, bob, 0, cost[bob])
 
-        return dis[n - 1][m - 1]
+        vis = [False] * n
+        vis[0] = True
+
+        queue = deque()
+        queue.append((0, 0, cost[0]))
+        max_ = None
+        while queue:
+            node, dep, weight = queue.popleft()
+
+            has_children = False
+            for nxt in adj[node]:
+                if vis[nxt]:
+                    continue
+                has_children = True
+                nxt_dep = dep + 1
+                nxt_weight = weight + cost[nxt]
+                if bob_dep[nxt] is not None:
+                    if bob_dep[nxt] < nxt_dep:
+                        nxt_weight -= cost[nxt]
+                    elif bob_dep[nxt] == nxt_dep:
+                        nxt_weight -= (cost[nxt] // 2)
+
+                vis[nxt] = True
+                queue.append((nxt, nxt_dep, nxt_weight))
+            if not has_children:
+                if max_:
+                    max_ = max(max_, weight)
+                else:
+                    max_ = weight
+
+        return max_
 
 
 if __name__ == '__main__':
     sol = Solution()
-
-    print(sol.minCost(
-        [[3, 4, 3], [2, 2, 2], [2, 1, 1], [4, 3, 2], [2, 1, 4], [2, 4, 1], [3, 3, 3], [1, 4, 2], [2, 2, 1], [2, 1, 1],
-         [3, 3, 1], [4, 1, 4], [2, 1, 4], [3, 2, 2], [3, 3, 1], [4, 4, 1], [1, 2, 2], [1, 1, 1], [1, 3, 4], [1, 2, 1],
-         [2, 2, 4], [2, 1, 3], [1, 2, 1], [4, 3, 2], [3, 3, 4], [2, 2, 1], [3, 4, 3], [4, 2, 3], [4, 4, 4]],
-    ))
+    print(sol.mostProfitablePath([[0, 1], [1, 2], [2, 3]], 3, [-5644, -6018, 1188, -8502]))
