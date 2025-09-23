8a197418e2a6929238c1d0fe4065786afbc492a4
khang00 <dlqkhang8@gmail.com>
2025-09-23 20:28:09 +0700

    problems

--- program.cpp
+++ program.cpp
@@ -11,10 +11,10 @@ using namespace __gnu_pbds;
 #define ul unsigned long
 #define ld long double
 #define fastio()                                                               \
-  ios_base::sync_with_stdio(false);                                            \
-  cin.tie(NULL);                                                               \
-  cout.tie(NULL);                                                              \
-  cout << fixed << std::setprecision(10)
+ios_base::sync_with_stdio(false);                                            \
+cin.tie(NULL);                                                               \
+cout.tie(NULL);                                                              \
+cout << fixed << std::setprecision(10)
 
 void IN_OUT() {
 #ifndef ONLINE_JUDGE
@@ -23,33 +23,38 @@ void IN_OUT() {
 #endif
 }
 
-vector<ll> getDigits(ll n) {
-  vector<ll> digits;
-  while (n > 0) {
-    ll mod = n % 10;
-    n /= 10;
+ll solve(ll n, vector<vector<char> > &grid) {
+  ll MOD = 1e9 + 7;
 
-    digits.push_back(mod);
-  }
-  std::reverse(digits.begin(), digits.end());
-  return digits;
-}
+  vector<vector<ll> > dp(n, vector<ll>(n, 0));
+  if (grid[0][0] != '*')
+    dp[0][0] = 1;
 
-ll solve(string n) {
-  ll mod = 10;
-  ll cnt = 0;
+  for (auto i = 0; i < n; i++)
+    for (auto j = 0; j < n; j++) {
+      if (grid[i][j] != '*' && i - 1 >= 0)
+        dp[i][j] = dp[i - 1][j] % MOD;
 
-  return cnt;
+      if (grid[i][j] != '*' && j - 1 >= 0)
+        dp[i][j] += dp[i][j - 1] % MOD;
+    }
+
+  return dp[n - 1][n - 1] % MOD;
 }
 
 int main() {
   fastio();
   IN_OUT();
 
-  string n;
+  ll n;
   cin >> n;
 
-  cout << solve(n);
+  vector<vector<char> > grid(n, vector<char>(n, '.'));
+  for (auto i = 0; i < n; i++)
+    for (auto j = 0; j < n; j++)
+      cin >> grid[i][j];
+
+  cout << solve(n, grid);
 
   return 0;
 }
--- program.py
+++ program.py
@@ -7,65 +7,27 @@ from typing import List, Deque, Tuple, Dict
 
 
 class Solution:
-    def mostProfitablePath(self, edges: List[List[int]], bob: int, cost: List[int]) -> int:
-        n = len(edges) + 1
+    def checkPrimeFrequency(self, a: List[int]) -> bool:
+        n = len(a)
+        f = defaultdict(int)
 
-        adj = [[] for _ in range(n)]
-        for u, v in edges:
-            adj[u].append(v)
-            adj[v].append(u)
+        for c in a:
+            f[c] += 1
 
-        bob_dep = [None] * n
+        def isPrime(n: int) -> bool:
+            if n < 2:
+                return False
 
-        def dfs(par, node, time, cum):
-            nonlocal bob_dep, cost
-            if node == 0:
-                return True
+            for i in range(2, ceil(sqrt(n))):
+                if n % i == 0:
+                    return False
+            return True
 
-            for nxt in adj[node]:
-                if nxt == par:
-                    continue
-
-                if dfs(node, nxt, time + 1, cum + cost[nxt]):
-                    bob_dep[nxt] = time + 1
-                    return True
-
-        bob_dep[bob] = 0
-        dfs(-1, bob, 0, cost[bob])
-
-        vis = [False] * n
-        vis[0] = True
-
-        queue = deque()
-        queue.append((0, 0, cost[0]))
-        max_ = None
-        while queue:
-            node, dep, weight = queue.popleft()
-
-            has_children = False
-            for nxt in adj[node]:
-                if vis[nxt]:
-                    continue
-                has_children = True
-                nxt_dep = dep + 1
-                nxt_weight = weight + cost[nxt]
-                if bob_dep[nxt] is not None:
-                    if bob_dep[nxt] < nxt_dep:
-                        nxt_weight -= cost[nxt]
-                    elif bob_dep[nxt] == nxt_dep:
-                        nxt_weight -= (cost[nxt] // 2)
-
-                vis[nxt] = True
-                queue.append((nxt, nxt_dep, nxt_weight))
-            if not has_children:
-                if max_:
-                    max_ = max(max_, weight)
-                else:
-                    max_ = weight
-
-        return max_
-
+        for k in f.keys:
+            if not isPrime(f[k]):
+                return False
+        return True
 
 if __name__ == '__main__':
     sol = Solution()
-    print(sol.mostProfitablePath([[0, 1], [1, 2], [2, 3]], 3, [-5644, -6018, 1188, -8502]))
+    print(sol.checkPrimeFrequency([[0, 1], [1, 2], [2, 3]], 3, [-5644, -6018, 1188, -8502]))
