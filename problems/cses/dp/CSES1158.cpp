054a8a2e8a0a3de6b0872e13799c45d21050243f
khang00 <dlqkhang8@gmail.com>
2025-10-18 21:22:49 +0700

    problems

--- problems/cses/dp/CSES1158.cpp
+++ problems/cses/dp/CSES1158.cpp
@@ -18,41 +18,41 @@ cout << fixed << std::setprecision(10)
 
 void IN_OUT() {
 #ifndef ONLINE_JUDGE
-  freopen("in.txt", "r", stdin);
-  freopen("out.txt", "w", stdout);
+    freopen("in.txt", "r", stdin);
+    freopen("out.txt", "w", stdout);
 #endif
 }
 
 ll solve(ll n, ll x, vector<int> &h, vector<int> &s) {
-  vector<vector<int>> dp(n + 1, vector<int>(x + 1, -1));
-  for (int j = 0; j <= x; j++)
-    dp[0][j] = 0;
+    vector<vector<int>> dp(n + 1, vector<int>(x + 1, -1));
+    for (int j = 0; j <= x; j++)
+        dp[0][j] = 0;
 
-  for (int i = 1; i <= n; i++)
-    for (int j = 0; j <= x; j++) {
-      dp[i][j] = dp[i - 1][j];
-      if (j - h[i - 1] >= 0)
-        dp[i][j] = max(dp[i][j], dp[i - 1][j - h[i - 1]] + s[i - 1]);
-    }
+    for (int i = 1; i <= n; i++)
+        for (int j = 0; j <= x; j++) {
+            dp[i][j] = dp[i - 1][j];
+            if (j - h[i - 1] >= 0)
+                dp[i][j] = max(dp[i][j], dp[i - 1][j - h[i - 1]] + s[i - 1]);
+        }
 
-  return dp[n][x];
+    return dp[n][x];
 }
 
 int main() {
-  fastio();
-  IN_OUT();
+    fastio();
+    IN_OUT();
 
-  ll n, x;
-  cin >> n >> x;
+    ll n, x;
+    cin >> n >> x;
 
-  vector<int> h(n, 0), s(n, 0);
-  for (auto i = 0; i < n; i++)
-    cin >> h[i];
+    vector<int> h(n, 0), s(n, 0);
+    for (auto i = 0; i < n; i++)
+        cin >> h[i];
 
-  for (auto i = 0; i < n; i++)
-    cin >> s[i];
+    for (auto i = 0; i < n; i++)
+        cin >> s[i];
 
-  cout << solve(n, x, h, s);
+    cout << solve(n, x, h, s);
 
-  return 0;
+    return 0;
 }
